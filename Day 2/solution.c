88cb069b0230bb134994dce2aef4fb71a9eaede9
Yoav Ravid <yoav.rvd@gmail.com>
2023-04-07 19:35:54 +0300

    Part 2 Day 2

--- Day 2/solution.c
+++ Day 2/solution.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
-enum rock_paper_scissors{
-    ROCK=0,
-    PAPER=1,
-    SCISSORS=2
-};
 
-                        //A B C
-int SCORETABLE[3][3] = { // Table of game result [opponent][me]
-                        {1+3,1+0,1+6}, // X
-                        {2+6,2+3,2+0}, // Y
-                        {3+0,3+6,3+3}};// Z
+int SCORETABLE_RPS[3][3] = { // Table of game result [opponent][me] for rock-paper-scissor
+                                //X   Y   Z 
+                                {1+3,2+6,3+0}, // A
+                                {1+0,2+3,3+6}, // B
+                                {1+6,2+0,3+3}};// C
+
+
+int SCORETABLE_LDW[3][3] = { // Table of game result [opponent][me] for lose-draw-win
+                        //X   Y   Z 
+                        {3+0,1+3,2+6}, // A
+                        {1+0,2+3,3+6}, // B
+                        {2+0,3+3,1+6}};// C
+
+
+int (*SCORETABLE)[3] = SCORETABLE_LDW; //{{0,0,0},{0,0,0},{0,0,0}}; // scoreboard
 
 
 int main(int argc, char *argv[]) {
@@ -20,6 +25,8 @@ int main(int argc, char *argv[]) {
     int score=0;
     int score_opp=0;
     int i=0,j=0;
+
+
     double mean_score = 0;
     for(i=0;i<3;i++) for(j=0;j<3;j++) mean_score += SCORETABLE[i][j];
     mean_score/=9;
