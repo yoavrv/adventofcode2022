6c16b99afb0686cd89ac68f9b743f3f9171ded69
Yoav Ravid <yoav.rvd@gmail.com>
2023-04-11 01:56:14 +0300

    Getting closer

--- Day 4/day_4/src/main.rs
+++ Day 4/day_4/src/main.rs
@@ -1,10 +1,26 @@
 use std::env;
 use std::fs;
+use std::io::{self, BufReader, BufRead};
 
+
+fn parse_line(line: &str) -> (u32,u32,u32,u32){
+    let v = line.splitn(4, |c| c == '-' || c == ',')
+                .map(|x| x.parse::<u32>().unwrap()) //turbofish to turn to numbers
+                .collect::<Vec<u32>>();
+    (v[0],v[1],v[2],v[3])
+}
+
+// stolen from stackoverflow https://stackoverflow.com/a/49964042
+// Reads from file in first argument or stdin
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let file: Option<&str> = args.get(1)
-    match file {
-        Some(file) => 
+    let input = env::args().nth(1);
+    let reader: Box<dyn BufRead> = match input {
+        None => Box::new(BufReader::new(io::stdin())),
+        Some(filename) => Box::new(BufReader::new(fs::File::open(filename).unwrap()))
+    };
+    for line in reader.lines() {
+        println!("{:?}", line);
+        println!("{:?}", parse_line(&line.unwrap()));
     }
 }
+
