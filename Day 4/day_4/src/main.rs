bda812b2ade1a1497403e6bac4f50cd6ac3ac036
Yoav Ravid <yoav.rvd@gmail.com>
2023-04-10 23:05:52 +0300

    Better attempt to get rust gitignore

---
+++ Day 4/day_4/src/main.rs
@@ -0,0 +1,10 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file: Option<&str> = args.get(1)
+    match file {
+        Some(file) => 
+    }
+}
