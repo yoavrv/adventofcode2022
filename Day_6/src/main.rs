6f45b53645383246252c7ae2b0f5ba3ce0771615
Yoav Ravid <yoav.rvd@gmail.com>
2023-04-12 00:44:37 +0300

    Works for part 2 but a little lazy

--- Day_6/src/main.rs
+++ Day_6/src/main.rs
@@ -2,19 +2,17 @@ use std::env;
 use std::fs;
 use std::io::{self, BufRead, BufReader};
 use std::usize;
+use std::collections::HashSet;
 
-fn parse_move_line(line: &str) -> Option<(usize, usize, usize)> {
-    // line is format "move x from y to z"
-    let v: Vec<usize> = line
-        .splitn(6," ") // ['move' 'x' 'from' 'y' 'to' 'z']
-        .skip(1)       // ['x' 'from' 'y' 'to' 'z']
-        .step_by(2)    // ['x' 'y' 'z']
-        .map(|x| x.parse::<usize>().unwrap()) //turbofish to turn to numbers
-        .collect();
-    match v.len() {
-        3 => Some((v[0], v[1], v[2])),
-        _ => None
+fn update_array<const N:usize>(array: &mut [char; N],c: char){
+    for i in 0..(array.len()-1){
+        array[i] = array[i+1];
     }
+    array[array.len()-1]=c;
+}
+
+fn array_has_duplicates<const N:usize>(array: [char; N]) -> bool{
+    HashSet::from(array).len()<N
 }
 
 // stolen from stackoverflow https://stackoverflow.com/a/49964042
@@ -34,19 +32,27 @@ fn main() {
     let reader = stdin_or_first_reader();
     let string = reader.lines().nth(0).unwrap();
     let (mut a,mut b,mut c,mut d) = (' ',' ',' ',' ');
+    let mut past_chars: [char; 14] = [' '; 14] ;
     println!("{:?}",string);
-    let mut i:usize = 1;
-    for new_char in string.unwrap().chars() {
+    let mut start:usize = 0;
+    let mut mess:usize = 0;
+    for (i, new_char) in string.unwrap().chars().enumerate() {
         a=b;
         b=c;
         c=d;
         d=new_char;
-        if a==b || a==c || a==d || b==c || b==d || c==d || a==' '{
-            i+=1;
+        if 0!=start || a==b || a==c || a==d || b==c || b==d || c==d || a==' '{
         }
         else {
-            break;
+           start=i+1;
+        }
+        update_array(&mut past_chars, new_char);
+        if mess!=0 || i<=past_chars.len() || array_has_duplicates(past_chars){
+
+        }
+        else {
+            mess=i+1;
         }
     }
-    println!("we start at {i}");
+    println!("Transmission starts at {start}; message starts as {mess}");
 }
