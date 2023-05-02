4854d1620c02b14bbfd4008f96f898b40644f88c
Yoav Ravid <yoav.rvd@gmail.com>
2023-05-03 01:58:03 +0300

    Day 10 part 2

--- day_10/solution.cpp
+++ day_10/solution.cpp
@@ -17,20 +17,25 @@ int main(int argc, char* argv[]) {
     } else {
         read_to_vec(std::cin,vec);
     }
-    int sum=1;
+    int accX=1;
+    int lit=0;
     for (int i=0; i<vec.size(); i++) {
-        if (i%20==0){
+        if (i%40==0){
              std::cout << "[";
         }
-        sum+=vec[i];
-        std::cout << vec[i] << " ";
-        if (i%20==19){
-             std::cout << "] => sum "<<sum<<"\n";
+        lit = accX-(i%40);
+        if (-1<=lit && lit<=1){
+            std::cout << "#";
+        } else {
+            std::cout << ".";
+        }
+        accX+=vec[i];
+        if (i%40==39){
+             std::cout << "] \n";
         }
     }
     std::cout << std::endl;
     std::cout << "signals" << std::endl;
-    int accX=0;
     int signal_sum=0;
     for (int i=20; i<vec.size() && i<240; i+=40) {
         accX = (std::accumulate(vec.begin(), vec.begin()+i-1, 1));
@@ -44,7 +49,7 @@ int main(int argc, char* argv[]) {
 void read_to_vec(std::istream& fin, vector<int>& vec){
     string line;
     while (getline(fin,line)){
-        std::cout << line << std::endl;
+        // std::cout << line << std::endl;
         if (line.find("addx")!=std::string::npos){
             vec.push_back(0);
             vec.push_back(std::stoi(line.substr(5,line.length())));
