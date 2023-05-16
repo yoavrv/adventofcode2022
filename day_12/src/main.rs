ac362b24c67143330ee48d040d49ea13043e1a79
Yoav Ravid <yoav.rvd@gmail.com>
2023-05-16 23:29:48 +0300

    Solve Day 12 part 1

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -48,7 +48,7 @@ impl PartialEq for QueueItem {
 
 fn print_matrix(v: &Vec<char>, stride: usize) {
     for line in v.chunks(stride) {
-        println!("{:?}",line);
+        println!("{}",String::from_iter(line));
     }
 }
 
@@ -80,8 +80,8 @@ fn main() {
 
     }
     let (mut x_start, mut y_start, mut x_end, mut y_end):(usize,usize,usize,usize) = (0,0,0,0);
-    for (x, l) in v.chunks_exact(stride).enumerate(){
-        for (y, c) in l.iter().enumerate(){
+    for (y, l) in v.chunks_exact(stride).enumerate(){
+        for (x, c) in l.iter().enumerate(){
             match c {
                 'S'=> (x_start,y_start)=(x,y),
                 'E'=> (x_end,y_end)=(x,y),
@@ -90,8 +90,8 @@ fn main() {
         }
         
     }
-    v[x_start*stride+y_start]='a';
-    v[x_end*stride+y_end]='z';
+    v[x_start+stride*y_start]='a';
+    v[x_end+stride*y_end]='z';
     println!("we start at {:?} and want to end at {:?}",(x_start,y_start),(x_end,y_end));
     print_matrix(&v,stride);
 
@@ -110,89 +110,85 @@ fn main() {
     queue_dist.push(QueueItem { dist : d(x_start,y_start), dist_from: 0, x: x_start, y: y_start }) ;
 
     let push = |x:usize,y: usize, path: usize,prev_x: usize,prev_y: usize, queue_dist: &mut BinaryHeap<QueueItem>,prev_xy :  &mut Vec<Option<(usize,usize)>>| {
-        queue_dist.push(QueueItem { dist : d(x, y)+path, dist_from: 0, x, y}) ;
-        prev_xy[x*stride+y] = Some((prev_x,prev_y));
+        queue_dist.push(QueueItem { dist : d(x, y)+path, dist_from: path, x, y}) ;
+        prev_xy[x+stride*y] = Some((prev_x,prev_y));
     };
 
     let find_xy = |x:usize,y:usize, prev_xy: &Vec<Option<(usize,usize)>>| {
         if x==x_start && y==y_start {return true;};
-        for (x3, part) in prev_xy.chunks(stride).enumerate() {
-            for (y3, item) in part.iter().enumerate() {
-                if let Some( (x2,y2) ) = item {
-                    if x==*x2 && y==*y2 {return true;};
-                    if x==x3 && y==y3 {return true;};
-                };
-            }
+        // for (y3, part) in prev_xy.chunks_exact(stride).enumerate() {
+        //     for (x3, item) in part[x.saturating_sub(2)..stride.min(x+2)].iter().enumerate() {
+        //         if let Some( (x2,y2) ) = item {
+        //             if x==*x2 && y==*y2 {return true;};
+        //             if x==x3 && y==y3 {return true;};
+        //         };
+        //     }
+        // }
+        match prev_xy[x+stride*y]{
+            Some(_tup) => return true,
+            None => return false,
         }
-        return false;
     };
 
 
-    println!("looking for (2,3): {}, looking for (2,4) {}",find_xy(2,3,&prev_xy),find_xy(2,4,&prev_xy));
-
+    let mut dist=0;
     let mut success=false;
     while !queue_dist.is_empty() && queue_dist.len()<v.len() {
         if let Some(cur_item) = queue_dist.pop() {
+            // println!("At {:?}",cur_item);
             if cur_item.x==x_end && cur_item.y==y_end {
                 success=true;
+                dist=cur_item.dist_from;
                 break;
             }
-            let (x,y, dist_from) : (usize,usize,usize)= (cur_item.x, cur_item.y, cur_item.dist_from);
-            let index: usize = x*stride+y;
+            let (x,y, dist_from) : (usize,usize,usize)= (cur_item.x, cur_item.y, cur_item.dist_from+1);
+            let index: usize = x+stride*y;
             let c: char = v[index];
             if 0<x {
-                let c2: char = v[index-stride];
+                let c2: char = v[index-1];
                 if ((c2 as u32)<=1+(c as u32)) && !find_xy(x-1,y,&prev_xy) {
                     push(x-1, y, dist_from,x, y, &mut queue_dist, &mut prev_xy);
                 }
             };
             if 0<y {
-                let c2 = v[index-1];
+                let c2 = v[index-stride];
                 if ((c2 as u32)<=1+(c as u32)) && !find_xy(x,y-1,&prev_xy) {
                     push(x, y-1, dist_from,x, y, &mut queue_dist, &mut prev_xy);
                 }
             };
-            if x<height-1 {
-                let c2 = v[index+stride];
+            if x<stride-1 {
+                let c2 = v[index+1];
                 if ((c2 as u32)<=1+(c as u32)) && !find_xy(x+1,y,&prev_xy) {
                     push(x+1, y, dist_from,x, y, &mut queue_dist, &mut prev_xy);
                 }
             };
-            if y<stride-1 {
-                let c2 = v[index+1];
+            if y<height-1 {
+                let c2 = v[index+stride];
                 if ((c2 as u32)<=1+(c as u32)) && !find_xy(x,y+1,&prev_xy) {
                     push(x, y+1, dist_from,x, y, &mut queue_dist, &mut prev_xy);
                 }
             };
         }
     }
+
+
     println!("Success: {}",success);
     println!("heap: {:?}", queue_dist);
-    print_matrix2(&prev_xy, stride);
-    println!("distance: {}",queue_dist.peek().unwrap().dist_from);
-    let mut cur_item = prev_xy[x_end*stride+y_end];
+    if height<10 && stride<10 {print_matrix2(&prev_xy, stride);}
+    println!("distance: {}",dist);
+    let mut cur_item = prev_xy[x_end+stride*y_end];
     let (mut x1,mut y1) = (x_end, y_end);
     while cur_item.is_some() {
         if let Some(item) = cur_item {
             let (x,y) = item;
-            let index = x*stride+y;
-            if x<x1 {v[index]='v'};
-            if x1<x {v[index]='^'};
-            if y<y1 {v[index]='>'};
-            if y1<y {v[index]='<'};
+            let index = x+stride*y;
+            if x<x1 {v[index]='>'};
+            if x1<x {v[index]='<'};
+            if y<y1 {v[index]='v'};
+            if y1<y {v[index]='^'};
             (x1,y1)=(x,y);
             cur_item = prev_xy[index];
         }
     }
     print_matrix(&v,stride);
 }
-// abcdefghijklmnopqrstuvwxyz
-
-/*
-Sbcde
-jihgf
-klmEy
-ponwx
-qqbva
-rstua
- */
